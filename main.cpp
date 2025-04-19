d512b7519eb3a3f2c715c5e5f67259b92515f686
Kacper <kacper@Kacpers-MacBook-Pro.local>
2025-04-19 15:36:35 +0200

    Tests using catch2

--- main.cpp
+++ main.cpp
@@ -1,102 +1,39 @@
 #include "include/array.hpp"
 #include "include/queue.hpp"
 #include "include/stack.hpp"
-#include <cstdio>
+#include <catch2/catch_test_macros.hpp>
 
-int main(int argc, char *argv[]) {
+TEST_CASE("Arrays", "[array]") {
+  Array<int, 5> array;
 
-  Array<int, 5> arr;
-  Array<int, 0> emptyArr;
+  REQUIRE(array.length() == 5);
 
-  Queue<int> queue;
-  Stack<int> stack;
-
-  printf("5 Int Array\n");
-  {
-    printf("arr memory: %zu\n", arr.length() * sizeof(int));
-
-    // Print array
-    for (int i = 0; i < arr.length(); i++) {
-      printf("arr [%i]: %i \n", i, arr[i]);
-    }
-
-    // Add items
-    printf("\n");
-    printf("Add items\n");
-    for (int i = 0; i < arr.length(); i++) {
-      arr[i] = i;
-    }
-
-    printf("arr findIndex: %d\n", arr.findIndex(1));
-    printf("arr findIndex: %d\n", arr.findIndex(69));
-
-    // Print array
-    for (int i = 0; i < arr.length(); i++) {
-      printf("arr [%i]: %i \n", i, arr[i]);
-    }
-
-    printf("\n");
-    printf("Push\n");
-    arr.push(18);
-    printf("arr memory: %zu\n", arr.length() * sizeof(int));
-
-    for (int i = 0; i < arr.length(); i++) {
-      printf("arr [%i]: %i \n", i, arr[i]);
-    }
-
-    printf("\n");
-    printf("Pop return: %d\n", arr.pop());
-
-    printf("arr memory: %zu\n", arr.length() * sizeof(int));
-
-    for (int i = 0; i < arr.length(); i++) {
-      printf("arr [%i]: %i \n", i, arr[i]);
-    }
-
-    printf("\n");
-    printf("Insert item\n");
-    arr.insert(23);
-    for (int i = 0; i < arr.length(); i++) {
-      printf("arr [%i]: %i \n", i, arr[i]);
-    }
-    printf("arr memory: %zu\n", arr.length() * sizeof(int));
-
-    printf("\n");
-    printf("InsertAt item\n");
-    arr.insertAt(80, 5);
-    for (int i = 0; i < arr.length(); i++) {
-      printf("arr [%i]: %i \n", i, arr[i]);
-    }
-    printf("arr memory: %zu\n", arr.length() * sizeof(int));
+  SECTION("Array insert") {
+    array.insert(69);
+    REQUIRE(array.length() == 6);
+    REQUIRE(array[0] == 69);
   }
 
-  printf("\n");
-  printf("Queue test\n");
-  {
-    queue.enqueue(3);
-    printf("Current queue: %d\n", queue.peek());
-    queue.enqueue(8);
-    printf("Current queue: %d\n", queue.peek());
-    printf("Deque: %d\n", queue.deque());
-    printf("Current queue: %d\n", queue.peek());
-    printf("Deque: %d\n", queue.deque());
-    printf("Current queue: %d\n", queue.peek());
-    printf("Deque: %d\n", queue.deque());
-    printf("Current queue: %d\n", queue.peek());
+  SECTION("Array insertAt") {
+    array.insertAt(69, 2);
+    REQUIRE(array.length() == 6);
+    REQUIRE(array[2] == 69);
   }
 
-  printf("\n");
-  printf("Stack test\n");
-  {
-    stack.push(3);
-    printf("Current stack: %d\n", stack.peek());
-    stack.push(8);
-    printf("Current stack: %d\n", stack.peek());
-    printf("Deque: %d\n", stack.pop());
-    printf("Current stack: %d\n", stack.peek());
-    printf("Deque: %d\n", stack.pop());
-    printf("Current stack: %d\n", stack.peek());
-    printf("Deque: %d\n", stack.pop());
-    printf("Current stack: %d\n", stack.peek());
+  SECTION("Array push") {
+    array.push(69);
+    REQUIRE(array.length() == 6);
+    REQUIRE(array[5] == 69);
+  }
+
+  SECTION("Array findIndex") {
+    array.insertAt(69, 2);
+    REQUIRE(array.findIndex(69) == 2);
+  }
+
+  SECTION("Array pop") {
+    array.push(69);
+    REQUIRE(array.pop() == 69);
+    REQUIRE(array.length() == 5);
   }
 }
